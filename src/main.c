3af41900340ea06ec331c4ab707b165d9685a766
Dmitry Semenovich <dima.semenovich.01@icloud.com>
2021-03-12 14:07:16 +0300

    Configured a travis-ci

---
+++ src/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  CarFleet
+//
+//  Created by Dmitry Semenovich on 12.03.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
